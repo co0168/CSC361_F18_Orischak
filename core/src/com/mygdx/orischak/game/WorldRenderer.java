94e7e503cc8cd0f86eb0ea33d38c72504901eaa8
connor <co0168@cs.ship.edu>
2018-10-24 14:45:54 -0400

    not sure what i changed but if i dont push it ill lose it

--- core/src/com/mygdx/orischak/game/WorldRenderer.java
+++ core/src/com/mygdx/orischak/game/WorldRenderer.java
@@ -4,7 +4,11 @@ import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.mygdx.orischak.util.Constants;
-
+/**
+ * 
+ * @author co0168
+ *
+ */
 public class WorldRenderer implements Disposable
 {
 	private OrthographicCamera camera;
