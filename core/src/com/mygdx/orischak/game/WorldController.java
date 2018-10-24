d9e44071ca7b30657c2897be103ed30805a98e2a
connor <co0168@cs.ship.edu>
2018-10-24 14:16:21 -0400

    began WorldController.

---
+++ core/src/com/mygdx/orischak/game/WorldController.java
@@ -0,0 +1,12 @@
+package com.mygdx.orischak.game;
+/**
+ * This class allows the player to use computer
+ * controlls to control the main characters movement
+ * as well as handle physics of game objects.
+ * @author co0168
+ *
+ */
+public class WorldController
+{
+	
+}
