3379ebbe967cc218dd37e535e9d2e5e6bcddbf60
connor <co0168@cs.ship.edu>
2018-10-24 14:18:35 -0400

    finished issue 1 Worldcontroller

--- core/src/com/mygdx/orischak/game/WorldController.java
+++ core/src/com/mygdx/orischak/game/WorldController.java
@@ -8,5 +8,21 @@ package com.mygdx.orischak.game;
  */
 public class WorldController
 {
+	private static final String TAG = WorldController.class.getName();
 	
+	public WorldController()
+	{
+		
+	}
+	
+	private void init()
+	{
+		
+	}
+	
+	public void update(float deltaTime)
+	{
+		
+	}
+
 }
