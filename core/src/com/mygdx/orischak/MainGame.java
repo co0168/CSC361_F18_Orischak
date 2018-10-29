af5a5b9264ce00149d8c1588cd453cbe5e6927c7
connor <co0168@cs.ship.edu>
2018-10-29 13:21:36 -0400

    completed create() and render()

--- core/src/com/mygdx/orischak/MainGame.java
+++ core/src/com/mygdx/orischak/MainGame.java
@@ -1,8 +1,13 @@
 package com.mygdx.orischak;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.mygdx.orischak.game.WorldController;
 import com.mygdx.orischak.game.WorldRenderer;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
 public class MainGame implements ApplicationListener 
 {
@@ -14,7 +19,10 @@ public class MainGame implements ApplicationListener
 	@Override
 	public void create() 
 	{
-		// TODO Auto-generated method stub
+		//set libgdx log level to DEBUG
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
 
 	}
 
@@ -28,8 +36,10 @@ public class MainGame implements ApplicationListener
 	@Override
 	public void render() 
 	{
-		// TODO Auto-generated method stub
-
+		worldController.update(Gdx.graphics.getDeltaTime());
+		Gdx.gl.glClearColor(0x64/255.0f,0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		worldRenderer.render();
 	}
 
 	@Override
