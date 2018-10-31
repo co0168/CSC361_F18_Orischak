d06449c7bff2ae73f1766da8bc698071c0f6361c
Unknown <co0168@cs.ship.edu>
2018-10-30 23:34:25 -0400

    created assets class and added texture atlas location to constants

---
+++ core/src/com/mygdx/orischak/game/Assets.java
@@ -0,0 +1,21 @@
+package com.mygdx.orischak.game;
+
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.utils.Disposable;
+
+public class Assets implements Disposable, AssetErrorListener {
+
+	@Override
+	public void error(AssetDescriptor asset, Throwable throwable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
