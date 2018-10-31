d1ed3b466b89cd763cab3d2ea5107e1fd8277fe4
connor <co0168@cs.ship.edu>
2018-10-31 13:51:03 -0400

    prepared the level

--- core/src/com/mygdx/orischak/util/Constants.java
+++ core/src/com/mygdx/orischak/util/Constants.java
@@ -8,4 +8,6 @@ public class Constants
 	public static final float VIEWPORT_HEIGHT = 5.0f;
 	// location of the texture atlas
 	public static final String TEXTURE_ATLAS_OBJECTS = "images/icehorse.atlas";
+	//location of the file for level 01
+	public static final String LEVEL_01 = "levels/level-01.png";
 }
