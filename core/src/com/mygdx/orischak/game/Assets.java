b6f86335f334642bc0e7354deb309e37d9be717f
Unknown <co0168@cs.ship.edu>
2018-10-31 00:07:15 -0400

    finished asset loading/handling. left off on pg 159

--- core/src/com/mygdx/orischak/game/Assets.java
+++ core/src/com/mygdx/orischak/game/Assets.java
@@ -3,19 +3,168 @@ package com.mygdx.orischak.game;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetErrorListener;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.mygdx.orischak.util.Constants;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+/**
+ * This class manages the assets
+ * and organizes them.
+ * @author Connor
+ *
+ */
+public class Assets implements Disposable, AssetErrorListener 
+{
 
-public class Assets implements Disposable, AssetErrorListener {
+	public static final String TAG = Assets.class.getName();
+	public static final Assets instance = new Assets();
+	private AssetManager assetManager;
 
+	public AssetGlaceon glaceon;
+	public AssetShelf shelf;
+	public AssetGoldCoin goldCoin;
+	public AssetPlanetCookie planetCookie;
+	public AssetLevelDecoration levelDecoration;
+	// singleton: prevent instantiation from other classes
+	private Assets () {}
+	public void init (AssetManager assetManager)
+	{
+		this.assetManager = assetManager;
+		// set asset manager error handler
+		assetManager.setErrorListener(this);
+		// load texture atlas
+		assetManager.load(Constants.TEXTURE_ATLAS_OBJECTS,
+				TextureAtlas.class);
+		// start loading assets and wait until finished
+		assetManager.finishLoading();
+		Gdx.app.debug(TAG, "# of assets loaded: "
+				+ assetManager.getAssetNames().size);
+		for (String a : assetManager.getAssetNames())
+		{
+			Gdx.app.debug(TAG, "asset: " + a);
+		}
+
+
+		TextureAtlas atlas = assetManager.get(Constants.TEXTURE_ATLAS_OBJECTS);
+
+		// enable texture filtering for pixel smoothing
+		for (Texture t : atlas.getTextures())
+		{
+			t.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		}
+
+		// create game resource objects
+		glaceon = new AssetGlaceon(atlas);
+		shelf = new AssetShelf(atlas);
+		goldCoin = new AssetGoldCoin(atlas);
+		planetCookie = new AssetPlanetCookie(atlas);
+		levelDecoration = new AssetLevelDecoration(atlas);
+	}
+
+
+	/**
+	 * Finds the glaceon picture in the atlas.
+	 * @author Connor
+	 *
+	 */
+	public class AssetGlaceon
+	{
+		public final AtlasRegion glaceon;
+
+		public AssetGlaceon(TextureAtlas atlas)
+		{
+			glaceon = atlas.findRegion("glaceon");
+		}
+	}
+
+	/**
+	 * Find the ice rocks.
+	 * @author Connor
+	 *
+	 */
+	public class AssetShelf
+	{
+		public final AtlasRegion edge;
+		public final AtlasRegion middle;
+
+		public AssetShelf(TextureAtlas atlas)
+		{
+			edge = atlas.findRegion("ice_edge");
+			middle = atlas.findRegion("ice_middle");
+		}
+	}
+
+	/**
+	 * Finds the gold coin
+	 * @author Connor
+	 *
+	 */
+	public class AssetGoldCoin
+	{
+		public final AtlasRegion goldCoin;
+		public AssetGoldCoin (TextureAtlas atlas) 
+		{
+			goldCoin = atlas.findRegion("item_gold_coin");
+		}
+	}
+
+	/**
+	 * Finds the planet cookie
+	 * @author Connor
+	 *
+	 */
+	public class AssetPlanetCookie
+	{
+		public final AtlasRegion planetCookie;
+
+		public AssetPlanetCookie(TextureAtlas atlas)
+		{
+			planetCookie = atlas.findRegion("planet_cookie");
+		}
+	}
+
+	/**
+	 * Finds all the level decorations
+	 * @author Connor
+	 *
+	 */
+	public class AssetLevelDecoration {
+		public final AtlasRegion cloud01;
+		public final AtlasRegion cloud02;
+		public final AtlasRegion cloud03;
+		public final AtlasRegion mountainLeft;
+		public final AtlasRegion mountainRight;
+		public final AtlasRegion waterOverlay;
+		public AssetLevelDecoration (TextureAtlas atlas) {
+			cloud01 = atlas.findRegion("cloud01");
+			cloud02 = atlas.findRegion("cloud02");
+			cloud03 = atlas.findRegion("cloud03");
+			mountainLeft = atlas.findRegion("mountain_left");
+			mountainRight = atlas.findRegion("mountain_right");
+			waterOverlay = atlas.findRegion("water_overlay");
+		}
+	}
 	@Override
-	public void error(AssetDescriptor asset, Throwable throwable) {
-		// TODO Auto-generated method stub
-
+	public void error(AssetDescriptor asset, Throwable throwable)
+	{
+		Gdx.app.error(TAG, "Couldn't load asset '"
+				+ asset.fileName + "'", (Exception)throwable);
 	}
 
 	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
+	public void dispose()
+	{
+		assetManager.dispose();
 	}
 
 }
+
+
+
+
+
+
+
