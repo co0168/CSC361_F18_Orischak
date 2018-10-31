68ea766906a3a0309d6b378687ab618cbc6575e9
Unknown <co0168@cs.ship.edu>
2018-10-30 23:13:57 -0400

    fixed issue where the packed image did not end up in the right place

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -20,7 +20,7 @@ public class DesktopLauncher
 			settings.maxHeight = 1024;
 			settings.duplicatePadding = false;
 			settings.debug = drawDebugOutline;
-			TexturePacker.process(settings, "assets-raw/images", "..core/assets/images", "icehorse");
+			TexturePacker.process(settings, "assets-raw/images", "../core/assets/images", "icehorse");
 		}
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Ice Horse";
