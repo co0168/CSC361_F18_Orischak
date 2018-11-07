845337dd921132f45a4d1afa4d9291f1c88cbd9f
Unknown <co0168@cs.ship.edu>
2018-11-07 13:56:56 -0500

    added fonts for use later in the menu

--- core/src/com/mygdx/orischak/game/Assets.java
+++ core/src/com/mygdx/orischak/game/Assets.java
@@ -10,6 +10,7 @@ import com.mygdx.orischak.util.Constants;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 /**
  * This class manages the assets
  * and organizes them.
@@ -28,6 +29,7 @@ public class Assets implements Disposable, AssetErrorListener
 	public AssetGoldCoin goldCoin;
 	public AssetPlanetCookie planetCookie;
 	public AssetLevelDecoration levelDecoration;
+	public AssetFonts fonts;
 	// singleton: prevent instantiation from other classes
 	private Assets () {}
 	public void init (AssetManager assetManager)
@@ -57,6 +59,7 @@ public class Assets implements Disposable, AssetErrorListener
 		}
 
 		// create game resource objects
+		fonts = new AssetFonts();
 		glaceon = new AssetGlaceon(atlas);
 		shelf = new AssetShelf(atlas);
 		goldCoin = new AssetGoldCoin(atlas);
@@ -147,20 +150,51 @@ public class Assets implements Disposable, AssetErrorListener
 			waterOverlay = atlas.findRegion("water_overlay");
 		}
 	}
-	@Override
-	public void error(AssetDescriptor asset, Throwable throwable)
-	{
-		Gdx.app.error(TAG, "Couldn't load asset '"
-				+ asset.fileName + "'", (Exception)throwable);
-	}
 
-	@Override
-	public void dispose()
+	public class AssetFonts 
 	{
-		assetManager.dispose();
+		public final BitmapFont defaultSmall;
+		public final BitmapFont defaultNormal;
+		public final BitmapFont defaultBig;
+		public AssetFonts () 
+		{
+			// create three fonts using Libgdx's 15px bitmap font
+			defaultSmall = new BitmapFont(
+					Gdx.files.internal("images/arial-15.fnt"), true);
+			defaultNormal = new BitmapFont(
+					Gdx.files.internal("images/arial-15.fnt"), true);
+			defaultBig = new BitmapFont(
+					Gdx.files.internal("images/arial-15.fnt"), true);
+			// set font sizes
+			defaultSmall.getData().setScale(0.75f);
+			defaultNormal.getData().setScale(1.0f);
+			defaultBig.getData().setScale(2.0f);
+			// enable linear texture filtering for smooth fonts
+			defaultSmall.getRegion().getTexture().setFilter(
+					TextureFilter.Linear, TextureFilter.Linear);
+			defaultNormal.getRegion().getTexture().setFilter(
+					TextureFilter.Linear, TextureFilter.Linear);
+			defaultBig.getRegion().getTexture().setFilter(
+					TextureFilter.Linear, TextureFilter.Linear);
+		}
 	}
+		@Override
+		public void error(AssetDescriptor asset, Throwable throwable)
+		{
+			Gdx.app.error(TAG, "Couldn't load asset '"
+					+ asset.fileName + "'", (Exception)throwable);
+		}
 
-}
+		@Override
+		public void dispose()
+		{
+			assetManager.dispose();
+			fonts.defaultSmall.dispose();
+			fonts.defaultNormal.dispose();
+			fonts.defaultBig.dispose();
+		}
+
+	}
 
 
 
