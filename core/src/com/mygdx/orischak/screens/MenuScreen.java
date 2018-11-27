997e5678a8e94d5f5ac57069b9a224b4d7b298af
Unknown <co0168@cs.ship.edu>
2018-11-27 17:47:55 -0500

    began working on screens

---
+++ core/src/com/mygdx/orischak/screens/MenuScreen.java
@@ -0,0 +1,53 @@
+package com.mygdx.orischak.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+public class MenuScreen extends AbstractGameScreen 
+{
+	private static final String TAG = MenuScreen.class.getName();
+
+	public MenuScreen (Game game)
+	{
+		super(game);
+	}
+
+	@Override
+	public void render(float deltaTime) 
+	{
+		Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		if(Gdx.input.isTouched())
+			game.setScreen(new GameScreen(game));
+	}
+
+	@Override
+	public void resize(int width, int height)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
